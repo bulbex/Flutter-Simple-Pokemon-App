f696821da188a0cf87a9279d49235a144d625da6
bulbex <bulbex24@gmail.com>
2023-07-19 13:46:26 +0200

    - Add files for tests - Some refactor

--- lib/entity/pokemon.dart
+++ lib/entity/pokemon.dart
@@ -12,28 +12,4 @@ class Pokemon {
     required this.weight,
     required this.height
   });
-
-  factory Pokemon.fromJson(Map<String, dynamic> json) {
-    return Pokemon(
-      name: json['name'],
-      photo: json['photo'],
-      types: json['types'].map((type) => type['type']['name']).toList(),
-      weight: json['weight'],
-      height: json['height']
-    );
-  }
-
-  // Map<String, dynamic> toMap() {
-  //   return {
-  //     'name': name,
-  //     'types': types,
-  //     'weight': weight,
-  //     'height': height
-  //   };
-  // }
-
-  // @override
-  // String toString() {
-  //   return 'Pokemon{name: $name, types: $types, weight: $weight, height: $height}';
-  // }
 }
--- lib/utils/network_connection.dart
+++ lib/utils/network_connection.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 
 Future<bool> checkConnection() async {
   try {
-    final result = await http.get(Uri.parse('http://example.com'));
+    final result = await http.get(Uri.parse('https://example.com'));
     return result.statusCode == 200 ? true : false;
   } on SocketException catch (_) {
     return false;
